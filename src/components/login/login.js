bb8b14ba631dfef47a7b7598b7e5df49262b923a
hitenSharm <hiten.sharm1708@gmail.com>
2021-06-25 14:12:42 +0530

    making login

---
+++ src/components/login/login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./login.css";
+import Logo from "../../assets/images/Logo.png";
+
+const Login = () => {
+  return (
+    <div className="Background">
+      <div className="Rectangle">
+        <div className="LogoStyle">
+          <img src={Logo} alt="logo"></img>
+        </div>
+        <div className="Title">Welcome to SpinUp</div>
+        <div className="subTitle">Connect with GitHub to get started</div>
+        <button className="Button-primary">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" width="22px" alt="GitHub Logo"/>
+          <div className="text-style">Get started with GitHub</div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
