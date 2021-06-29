04f3694b099962fd7a150fb4f9f5c2c83ab43c4e
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-06-29 22:40:30 +0530

    Completed the specific cluster details page

--- src/components/clusterdetails/clusterdetails.js
+++ src/components/clusterdetails/clusterdetails.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Row, Col, Tag, Divider, Tabs } from "antd";
+import { Row, Col, Tag, Divider, Tabs, Input, Card, Button } from "antd";
 
 import {
   CopyOutlined,
@@ -8,11 +8,22 @@ import {
   SettingOutlined,
   LineChartOutlined,
   SyncOutlined,
+  EyeInvisibleOutlined,
+  EyeTwoTone,
 } from "@ant-design/icons";
 
 const { TabPane } = Tabs;
 
 const ClusterDetails = () => {
+  const [host, setHost] = useState("hktfgjaymvppxurjixty.spinup.host");
+  const [database, setDatabase] = useState("postgres");
+  const [port, setPort] = useState(5432);
+  const [username, setUsername] = useState("johndoe");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  useEffect(() => {
+    console.log(username);
+  }, [username]);
   return (
     <>
       <Row style={{ marginTop: "50px" }}>
@@ -78,7 +89,209 @@ const ClusterDetails = () => {
         <Col xxl={4} xl={4} lg={4} xs={2} md={1} sm={2} />
       </Row>
       <Divider style={{ backgroundColor: "#313b4d", marginTop: "-1px" }} />
-      <Row></Row>
+      <Row>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Col xxl={9} xl={9} lg={9} xs={20} md={10} sm={20}>
+          <p style={{ color: "#738095" }}>Host</p>
+          <Input
+            bordered={false}
+            style={{
+              backgroundColor: "#394150",
+              color: "white",
+              height: "60px",
+              borderRadius: "5px",
+            }}
+            disabled
+            value={host}
+          />
+        </Col>
+        <Col xxl={4} xl={4} lg={4} />
+        <Col xxl={4} xl={4} lg={4} xs={20} md={10} sm={20}>
+          <Card
+            style={{
+              width: "308px",
+              backgroundColor: "#283141",
+              border: "none",
+            }}
+            headStyle={{
+              borderBottom: "none",
+            }}
+            title={
+              <p style={{ color: "#6abfaf" }}>
+                Connect to your cluster <br /> from anywhere
+              </p>
+            }
+          >
+            <p style={{ color: "#a0aec8", marginTop: "-30px" }}>
+              Open Terminal on your device
+            </p>
+            <p style={{ color: "#a0aec8" }}>
+              Copy paste your host link & press enter
+            </p>
+            <p style={{ color: "#a0aec8" }}>Enter your password. voila!</p>
+            <p style={{ color: "#a0aec8" }}>Create as much as dbs you want!</p>
+          </Card>
+        </Col>
+        <Col xxl={4} xl={4} lg={4} xs={2} md={1} sm={2} />
+      </Row>
+      <Row style={{ marginTop: "-125px" }}>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Col xxl={4} xl={4} lg={4} xs={20} md={10} sm={20}>
+          <p style={{ color: "#738095" }}>Database Name</p>
+          <Input
+            bordered={false}
+            style={{
+              backgroundColor: "#394150",
+              color: "white",
+              height: "40px",
+              borderRadius: "5px",
+            }}
+            disabled
+            value={database}
+          />
+        </Col>
+        <Col xxl={1} xl={1} lg={1} xs={2} md={3} sm={2} />
+        <Col xxl={0} lg={0} xl={0} xs={2} md={3} sm={2} />
+        <Col xxl={4} xl={4} lg={4} xs={20} md={10} sm={20}>
+          <p style={{ color: "#738095" }}>Port</p>
+          <Input
+            bordered={false}
+            style={{
+              backgroundColor: "#394150",
+              color: "white",
+              height: "40px",
+              borderRadius: "5px",
+            }}
+            disabled
+            value={port}
+          />
+        </Col>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+      </Row>
+      <Row>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Col xxl={9} xl={9} lg={9} xs={20} md={10} sm={20}>
+          <Divider style={{ backgroundColor: "#313b4d", marginTop: "50px" }} />
+        </Col>
+        <Col xxl={9} xl={9} lg={9} md={8} />
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+      </Row>
+      <Row>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Col xxl={4} xl={4} lg={4} xs={20} md={10} sm={20}>
+          <p style={{ color: "#738095" }}>Username</p>
+          <Input
+            bordered={false}
+            style={{
+              backgroundColor: "#394150",
+              color: "white",
+              height: "40px",
+              borderRadius: "5px",
+            }}
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </Col>
+      </Row>
+      <Row style={{ marginTop: "25px" }}>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Col xxl={4} xl={4} lg={4} xs={20} md={10} sm={20}>
+          <p style={{ color: "#738095" }}>Enter new password</p>
+          <Input.Password
+            bordered={false}
+            style={{
+              backgroundColor: "#394150",
+              color: "white",
+              height: "40px",
+              borderRadius: "5px",
+            }}
+            iconRender={(visible) =>
+              visible ? (
+                <EyeTwoTone style={{ color: "white" }} />
+              ) : (
+                <EyeInvisibleOutlined style={{ color: "white" }} />
+              )
+            }
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </Col>
+        <Col xxl={1} xl={1} lg={1} xs={2} md={3} sm={2} />
+        <Card
+          bordered={false}
+          style={{
+            backgroundColor: "#212936",
+            color: "white",
+            lineHeight: "1",
+            borderLeft: "1px solid #283141",
+          }}
+        >
+          <p>Must be atleast :</p>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;10-16 alpha-numeric characters</p>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;1 uppercase letter (A,B,..Z)</p>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;1 lowercase letter (a,b,..z)</p>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;1 number (0-9)</p>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;1 special character(-,_,$,#,!)</p>
+        </Card>
+      </Row>
+      <Row style={{ marginTop: "-120px" }}>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Col xxl={4} xl={4} lg={4} xs={20} md={10} sm={20}>
+          <p style={{ color: "#738095" }}>Confirm password</p>
+          <Input.Password
+            bordered={false}
+            style={{
+              backgroundColor: "#394150",
+              color: "white",
+              height: "40px",
+              borderRadius: "5px",
+            }}
+            iconRender={(visible) =>
+              visible ? (
+                <EyeTwoTone style={{ color: "white" }} />
+              ) : (
+                <EyeInvisibleOutlined style={{ color: "white" }} />
+              )
+            }
+            value={confirm}
+            onChange={(e) => {
+              setConfirm(e.target.value);
+            }}
+          />
+        </Col>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+      </Row>
+      <Row style={{ marginTop: "25px" }}>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Button
+          type="text"
+          style={{
+            color: "black",
+            backgroundColor: "#42e8e0",
+          }}
+        >
+          <b>Save Changes</b>
+        </Button>
+        <Button
+          type="text"
+          style={{
+            color: "black",
+            backgroundColor: "#212936",
+          }}
+        >
+          <b style={{ color: "#42e8e0" }}>Cancel</b>
+        </Button>
+      </Row>
+      <Row>
+        <Col xxl={3} xl={3} lg={3} xs={2} md={3} sm={2} />
+        <Col xxl={9} xl={9} lg={9} xs={20} md={10} sm={20}>
+          <Divider style={{ backgroundColor: "#313b4d", marginTop: "30px" }} />
+        </Col>
+      </Row>
     </>
   );
 };
