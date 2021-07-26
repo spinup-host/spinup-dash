abcfa78e25227e77203fc8b838ed599750423ba8
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-07-26 21:04:59 +0530

    Added token as config headers

--- src/api/createService.js
+++ src/api/createService.js
@@ -4,17 +4,27 @@ import { createNotification } from "../components/notifications/notify";
 export const handleOk = async (name, database, version) => {
   //console.log(name, database, version);
   if (name.length === 0) {
-    // Error Notification    
-    createNotification("error","Please enter a cluster name.");
+    // Error Notification
+    createNotification("error", "Please enter a cluster name.");
     return 0;
   }
   try {
-    var response = await axios.post(`${process.env.REACT_APP_SERVER_URI}`, {
-      UserID: "replaceme",
-      ClusterName: name,
-      Name: database,
-      version: version,
-    });
+    var response = await axios.post(
+      `${process.env.REACT_APP_SERVER_URI}`,
+      {
+        UserID: JSON.parse(localStorage.getItem("details")).login,
+        ClusterName: name,
+        Name: database,
+        version: version,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem("details")).jwttoken
+          }`,
+        },
+      }
+    );
     return response;
   } catch (e) {
     console.log(e);
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -18,6 +18,7 @@ const Dashboard = () => {
       history.push("/");
     } else {
       userDetails = JSON.parse(localStorage.getItem("details"));
+      console.log(JSON.parse(localStorage.getItem("details")).login);
       // console.log(JSON.parse(localStorage.getItem("details")));
       setUser(userDetails);
     }
