d02f98303746d85233a3cd6bfd628d5d83c68902
hitenSharm <hiten.sharm1708@gmail.com>
2021-07-27 12:44:55 +0530

    fixed lazy loading bug

--- src/components/databases/dbsForDashboard.js
+++ src/components/databases/dbsForDashboard.js
@@ -1,47 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { Spin } from "antd";
 
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import "./db.css";
 
 const DatabaseForDashboard = (props) => {
   const dataBaseSelected = props.databaseSelected;
   const databaseRendering = props.databaseRendering;      
-  const [loadingImage,setLoading]=useState(true);
 
+  var postgresLink,mysqlLink,ectdLink;
   var imageLink="";  
   if(databaseRendering==="postgres"){
       imageLink="https://softwareengineeringdaily.com/wp-content/uploads/2016/10/PostgreSQL.png";
+      postgresLink=imageLink;
   }
   if(databaseRendering==="mysql"){
     imageLink="https://d1.awsstatic.com/asset-repository/products/amazon-rds/1024px-MySQL.ff87215b43fd7292af172e2a5d9b844217262571.png";
+    mysqlLink=imageLink;
   }
   if(databaseRendering==="ectd"){
       imageLink="https://dbdb.io/media/logos/etcd.png";
+      ectdLink=imageLink;
   }  
+  //checks if image has been cached
+  function isCached(src) {
+    var img = new Image();
+    img.src = src;
+    var complete = img.complete;
+    img.src = "";
+    return complete;
+}
 
   const renderDb = () => {    
     if (databaseRendering === dataBaseSelected) {        
       return (
-        <div className="databaseSelected">
-          {loadingImage ?<Spin/>:null}
+        <div className="databaseSelected">                            
            <LazyLoadImage
             alt={databaseRendering}
+            effect="blur"
             className="imageDb"            
             src={imageLink} // use normal <img> attributes as props
-            afterLoad={()=>setLoading(false)}          
+            threshold={120}                         
+            visibleByDefault={isCached(imageLink)}                            
           />                     
         </div>
       );
     } else {
       return(
-        <div className="databaseRendered">
-          {loadingImage ?<Spin/>:null}
+        <div className="databaseRendered">                                   
           <LazyLoadImage
             alt={databaseRendering}
-            className="imageDb"            
-            src={imageLink} // use normal <img> attributes as props
-            afterLoad={()=>setLoading(false)}      
+            effect="blur"    
+            className="imageDb"     
+            threshold={120}   
+            src={imageLink} // use normal <img> attributes as props            
+            visibleByDefault={isCached(imageLink)}                               
           />                  
         </div>
       );
