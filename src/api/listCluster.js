85f2259cae08c065cf8da17096057d49d78f1e62
idoko <okokomichaels@outlook.com>
2022-01-06 21:42:10 +0100

    store only server base uri in env variable

--- src/api/listCluster.js
+++ src/api/listCluster.js
@@ -3,7 +3,7 @@ import { createNotification } from "../components/notifications/notify";
 
 export const getClusters = async () => {
   try {
-    var response = await axios.get(process.env.REACT_APP_LIST_URI, {
+    let response = await axios.get(`${process.env.REACT_APP_SERVER_URI}/listcluster`, {
       headers: {
         Authorization: `Bearer ${
           JSON.parse(localStorage.getItem("details")).jwttoken
