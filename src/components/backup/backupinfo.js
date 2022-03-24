77f7307a3c9c0df85ee19c01a3f4e47a9c431312
Akshat Sharma <akshatsharma2602@gmail.com>
2022-03-24 22:42:51 +0530

    Fixed responsiveness as well

--- src/components/backup/backupinfo.js
+++ src/components/backup/backupinfo.js
@@ -37,7 +37,7 @@ const BackupInfo = () => {
       minute: backuptime.split(":")[1],
     };
     let response = await schedulebackup(backupdetails);
-    if (response.status == 200) {
+    if (response.status === 200) {
       createNotification("success", "Backup Scheduled");
     } else {
       createNotification("error", "Backup Failed");
@@ -122,10 +122,10 @@ const BackupInfo = () => {
       <Divider style={{ backgroundColor: "#313b4d", marginTop: "-1px" }} />
       <Row>
         <Col xxl={1} xl={1} lg={1} xs={1} md={1} sm={1} />
-        <Col xxl={16} xl={16} lg={16} xs={16} md={16} sm={16}>
+        <Col xxl={16} xl={16} lg={16} xs={22} md={22} sm={22}>
       <Row>
-        <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
-        <Col xxl={18} xl={18} lg={18} xs={20} md={20} sm={20}>
+        <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
+        <Col xxl={18} xl={18} lg={18} xs={22} md={22} sm={22}>
           <p style={{ color: "#738095" }}>Database Name</p>
           <Input
             bordered={false}
@@ -142,8 +142,8 @@ const BackupInfo = () => {
         <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
       </Row>
       <Row style={{marginTop: "1%"}}>
-        <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
-        <Col xxl={8} xl={8} lg={8} xs={9} md={9} sm={9}>
+        <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
+        <Col xxl={8} xl={8} lg={8} xs={22} md={22} sm={22}>
           <p style={{ color: "#738095" }}>Destination Name</p>
           <Input
             bordered={false}
@@ -156,8 +156,9 @@ const BackupInfo = () => {
             onChange={(e) => setDest(e.target.value)}
           />
         </Col>
-        <Col xxl={2} xl={2} lg={2} xs={1} md={2} sm={2} />
-        <Col xxl={8} xl={8} lg={8} xs={9} md={9} sm={9}>
+        <Col xxl={2} xl={2} lg={2} xs={1} md={1} sm={1} />
+        <Col xxl={0} xl={0} lg={0} xs={1} md={1} sm={1} />
+        <Col xxl={8} xl={8} lg={8} xs={22} md={22} sm={22}>
           <p style={{ color: "#738095" }}>Destination URL</p>
           <Input
             bordered={false}
@@ -170,13 +171,11 @@ const BackupInfo = () => {
             onChange={(e) => setDestUrl(e.target.value)}
           />
         </Col>
-        <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
+        <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
       </Row>
       <Row style={{marginTop: "1%"}}>
-      <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
-        <Col
-          xxl={8} xl={8} lg={8} xs={9} md={9} sm={9}
-        >
+      <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
+        <Col xxl={8} xl={8} lg={8} xs={22} md={22} sm={22}>
           <p style={{ color: "#738095" }}>API key ID</p>
           <Input
             bordered={false}
@@ -189,11 +188,9 @@ const BackupInfo = () => {
             onChange={(e) => setApiKey(e.target.value)}
           />
         </Col>
-        <Col xxl={2} xl={2} lg={2} xs={1} md={2} sm={2} />
-
-        <Col
-          xxl={8} xl={8} lg={8} xs={9} md={9} sm={9}
-        >
+        <Col xxl={2} xl={2} lg={2} xs={1} md={1} sm={1} />
+        <Col xxl={0} xl={0} lg={0} xs={1} md={1} sm={1} />
+        <Col xxl={8} xl={8} lg={8} xs={22} md={22} sm={22}>
           <p style={{ color: "#738095" }}>API key Secret</p>
           <Input
             bordered={false}
@@ -206,12 +203,11 @@ const BackupInfo = () => {
             onChange={(e) => setApiKeySecret(e.target.value)}
           />
         </Col>
+        <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
       </Row>
       <Row style={{marginTop:"1%"}}>
-      <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
-        <Col
-          xxl={8} xl={8} lg={8} xs={9} md={9} sm={9}
-        >
+      <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
+      <Col xxl={8} xl={8} lg={8} xs={22} md={22} sm={22}>
           <p style={{ color: "#738095" }}>Username</p>
           <Input
             bordered={false}
@@ -224,11 +220,10 @@ const BackupInfo = () => {
             onChange={(e) => setUsername(e.target.value)}
           />
         </Col>
-        <Col xxl={2} xl={2} lg={2} xs={1} md={2} sm={2} />
+        <Col xxl={2} xl={2} lg={2} xs={1} md={1} sm={1} />
+        <Col xxl={0} xl={0} lg={0} xs={1} md={1} sm={1} />
 
-        <Col
-          xxl={8} xl={8} lg={8} xs={9} md={9} sm={9}
-        >
+        <Col xxl={8} xl={8} lg={8} xs={22} md={22} sm={22}>
           <p style={{ color: "#738095" }}>Password</p>
           <Input
             bordered={false}
@@ -241,9 +236,10 @@ const BackupInfo = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Col>
+        <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
       </Row>
       <Row style={{marginTop: "1%"}}>
-      <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
+      <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
         <Col
           xxl={4}
           xl={4}
@@ -260,7 +256,7 @@ const BackupInfo = () => {
         </Col>
       </Row>
       <Row style={{marginTop: "1%"}}>
-        <Col xxl={3} xl={3} lg={3} xs={2} md={2} sm={2} />
+        <Col xxl={3} xl={3} lg={3} xs={1} md={1} sm={1} />
         <Col
           xxl={4}
           xl={4}
@@ -283,10 +279,11 @@ const BackupInfo = () => {
       </Row>
       <NotificationContainer />
       </Col>
-      <Col xxl={6} xl={6} lg={6} xs={6} md={6} sm={6}>
+      <Col xxl={0} xl={0} lg={0} xs={1} md={1} sm={1} />
+      <Col xxl={0} xl={0} lg={0} xs={2} md={2} sm={2} />
+      <Col style={{marginTop: "1%"}} xxl={6} xl={6} lg={6} xs={21} md={21} sm={21}>
       <Card
             style={{
-              // width: "308px",
               backgroundColor: "#283141",
               border: "none",
             }}
